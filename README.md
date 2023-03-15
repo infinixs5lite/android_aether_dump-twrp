f42622e2a97c78c46faad3303fff953e19bdd244
atharvnegi <atharvnegi2951@gmail.com>
2023-03-15 10:45:41 +0530

    Add extras for missi-user 12 SP1A.210812.016 V13.0.6.0.SCVEUXM release-keys
    Signed-off-by: atharvnegi <atharvnegi2951@gmail.com>

---
+++ README.md
@@ -0,0 +1,19 @@
+## missi-user 12 SP1A.210812.016 V13.0.6.0.SCVEUXM release-keys
+- Manufacturer: xiaomi
+- Platform: mt6768
+- Codename: aether
+- Brand: Android
+- Flavor: missi-user
+- Release Version: 12
+- Kernel Version: 4.19.191
+- Id: SP1A.210812.016
+- Incremental: V13.0.6.0.SCVEUXM
+- Tags: release-keys
+- CPU Abilist: arm64-v8a,armeabi-v7a,armeabi
+- A/B Device: true
+- Locale: en-GB
+- Screen Density: 440
+- Fingerprint: Android/vnd_earth/earth:12/SP1A.210812.016/V13.0.6.0.SCVEUXM:user/release-keys
+- OTA version: 
+- Branch: missi-user-12-SP1A.210812.016-V13.0.6.0.SCVEUXM-release-keys
+- Repo: android_aether_dump
---
+++ join_split_files.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+cat system/system/priv-app/MiuiCamera/MiuiCamera.apk.* 2>/dev/null >> system/system/priv-app/MiuiCamera/MiuiCamera.apk
+rm -f system/system/priv-app/MiuiCamera/MiuiCamera.apk.* 2>/dev/null
+cat system/system/priv-app/MiuiCamera/oat/arm64/MiuiCamera.odex.* 2>/dev/null >> system/system/priv-app/MiuiCamera/oat/arm64/MiuiCamera.odex
+rm -f system/system/priv-app/MiuiCamera/oat/arm64/MiuiCamera.odex.* 2>/dev/null
+cat system/system/priv-app/MIUIGalleryGlobalLite2/MIUIGalleryGlobalLite2.apk.* 2>/dev/null >> system/system/priv-app/MIUIGalleryGlobalLite2/MIUIGalleryGlobalLite2.apk
+rm -f system/system/priv-app/MIUIGalleryGlobalLite2/MIUIGalleryGlobalLite2.apk.* 2>/dev/null
+cat system/system/priv-app/MIBrowserGlobal/MIBrowserGlobal.apk.* 2>/dev/null >> system/system/priv-app/MIBrowserGlobal/MIBrowserGlobal.apk
+rm -f system/system/priv-app/MIBrowserGlobal/MIBrowserGlobal.apk.* 2>/dev/null
+cat system/system/priv-app/SecurityCenter/SecurityCenter.apk.* 2>/dev/null >> system/system/priv-app/SecurityCenter/SecurityCenter.apk
+rm -f system/system/priv-app/SecurityCenter/SecurityCenter.apk.* 2>/dev/null
+cat system/system/app/MIUIVideoPlayer/MIUIVideoPlayer.apk.* 2>/dev/null >> system/system/app/MIUIVideoPlayer/MIUIVideoPlayer.apk
+rm -f system/system/app/MIUIVideoPlayer/MIUIVideoPlayer.apk.* 2>/dev/null
+cat system/system/data-app/MIUINotes/MIUINotes.apk.* 2>/dev/null >> system/system/data-app/MIUINotes/MIUINotes.apk
+rm -f system/system/data-app/MIUINotes/MIUINotes.apk.* 2>/dev/null
+cat system/system/data-app/MIMediaEditorGlobal/MIMediaEditorGlobal.apk.* 2>/dev/null >> system/system/data-app/MIMediaEditorGlobal/MIMediaEditorGlobal.apk
+rm -f system/system/data-app/MIMediaEditorGlobal/MIMediaEditorGlobal.apk.* 2>/dev/null
+cat system/system/data-app/MISTORE_OVERSEA/MISTORE_OVERSEA.apk.* 2>/dev/null >> system/system/data-app/MISTORE_OVERSEA/MISTORE_OVERSEA.apk
+rm -f system/system/data-app/MISTORE_OVERSEA/MISTORE_OVERSEA.apk.* 2>/dev/null
+cat system/system/system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null >> system/system/system_ext/priv-app/Settings/Settings.apk
+rm -f system/system/system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null
+cat system/system/system_ext/apex/com.android.vndk.v30.apex.* 2>/dev/null >> system/system/system_ext/apex/com.android.vndk.v30.apex
+rm -f system/system/system_ext/apex/com.android.vndk.v30.apex.* 2>/dev/null
+cat product/priv-app/Velvet/Velvet.apk.* 2>/dev/null >> product/priv-app/Velvet/Velvet.apk
+rm -f product/priv-app/Velvet/Velvet.apk.* 2>/dev/null
+cat product/priv-app/Messages/Messages.apk.* 2>/dev/null >> product/priv-app/Messages/Messages.apk
+rm -f product/priv-app/Messages/Messages.apk.* 2>/dev/null
+cat product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null >> product/priv-app/GmsCore/GmsCore.apk
+rm -f product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null
+cat product/app/SpeechServicesByGoogle/SpeechServicesByGoogle.apk.* 2>/dev/null >> product/app/SpeechServicesByGoogle/SpeechServicesByGoogle.apk
+rm -f product/app/SpeechServicesByGoogle/SpeechServicesByGoogle.apk.* 2>/dev/null
+cat product/app/TrichromeLibrary/TrichromeLibrary.apk.* 2>/dev/null >> product/app/TrichromeLibrary/TrichromeLibrary.apk
+rm -f product/app/TrichromeLibrary/TrichromeLibrary.apk.* 2>/dev/null
+cat product/app/LatinImeGoogle/LatinImeGoogle.apk.* 2>/dev/null >> product/app/LatinImeGoogle/LatinImeGoogle.apk
+rm -f product/app/LatinImeGoogle/LatinImeGoogle.apk.* 2>/dev/null
+cat product/app/Gmail2/Gmail2.apk.* 2>/dev/null >> product/app/Gmail2/Gmail2.apk
+rm -f product/app/Gmail2/Gmail2.apk.* 2>/dev/null
+cat product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null >> product/app/WebViewGoogle/WebViewGoogle.apk
+rm -f product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null
+cat product/app/Maps/Maps.apk.* 2>/dev/null >> product/app/Maps/Maps.apk
+rm -f product/app/Maps/Maps.apk.* 2>/dev/null
+cat product/app/Photos/Photos.apk.* 2>/dev/null >> product/app/Photos/Photos.apk
+rm -f product/app/Photos/Photos.apk.* 2>/dev/null
+cat product/app/YouTube/YouTube.apk.* 2>/dev/null >> product/app/YouTube/YouTube.apk
+rm -f product/app/YouTube/YouTube.apk.* 2>/dev/null
+cat cust/app/customized/partner-com.amazon.mShop.android.shopping_41/partner-com.amazon.mShop.android.shopping_41.apk.* 2>/dev/null >> cust/app/customized/partner-com.amazon.mShop.android.shopping_41/partner-com.amazon.mShop.android.shopping_41.apk
+rm -f cust/app/customized/partner-com.amazon.mShop.android.shopping_41/partner-com.amazon.mShop.android.shopping_41.apk.* 2>/dev/null
+cat cust/app/customized/partner-com.amazon.mp3_123/partner-com.amazon.mp3_123.apk.* 2>/dev/null >> cust/app/customized/partner-com.amazon.mp3_123/partner-com.amazon.mp3_123.apk
+rm -f cust/app/customized/partner-com.amazon.mp3_123/partner-com.amazon.mp3_123.apk.* 2>/dev/null
+cat cust/app/customized/partner-com.booking_85/partner-com.booking_85.apk.* 2>/dev/null >> cust/app/customized/partner-com.booking_85/partner-com.booking_85.apk
+rm -f cust/app/customized/partner-com.booking_85/partner-com.booking_85.apk.* 2>/dev/null
+cat cust/app/customized/partner-com.ss.android.ugc.trill_101/partner-com.ss.android.ugc.trill_101.apk.* 2>/dev/null >> cust/app/customized/partner-com.ss.android.ugc.trill_101/partner-com.ss.android.ugc.trill_101.apk
+rm -f cust/app/customized/partner-com.ss.android.ugc.trill_101/partner-com.ss.android.ugc.trill_101.apk.* 2>/dev/null
+cat cust/app/customized/partner-com.kwai.video_33/partner-com.kwai.video_33.apk.* 2>/dev/null >> cust/app/customized/partner-com.kwai.video_33/partner-com.kwai.video_33.apk
+rm -f cust/app/customized/partner-com.kwai.video_33/partner-com.kwai.video_33.apk.* 2>/dev/null
+cat cust/app/customized/partner-com.snapchat.android_200/partner-com.snapchat.android_200.apk.* 2>/dev/null >> cust/app/customized/partner-com.snapchat.android_200/partner-com.snapchat.android_200.apk
+rm -f cust/app/customized/partner-com.snapchat.android_200/partner-com.snapchat.android_200.apk.* 2>/dev/null
+cat cust/app/customized/partner-com.facebook.katana_91/partner-com.facebook.katana_91.apk.* 2>/dev/null >> cust/app/customized/partner-com.facebook.katana_91/partner-com.facebook.katana_91.apk
+rm -f cust/app/customized/partner-com.facebook.katana_91/partner-com.facebook.katana_91.apk.* 2>/dev/null
+cat cust/app/customized/partner-com.kwai.kuaishou.video.live_155/partner-com.kwai.kuaishou.video.live_155.apk.* 2>/dev/null >> cust/app/customized/partner-com.kwai.kuaishou.video.live_155/partner-com.kwai.kuaishou.video.live_155.apk
+rm -f cust/app/customized/partner-com.kwai.kuaishou.video.live_155/partner-com.kwai.kuaishou.video.live_155.apk.* 2>/dev/null
+cat boot.img.* 2>/dev/null >> boot.img
+rm -f boot.img.* 2>/dev/null
